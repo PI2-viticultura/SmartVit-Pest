b501e0d0cb8559f5dde350b6c091901fdea1125c
Joao Lucas <joao.lucas.ssr@gmail.com>
2020-11-13 00:51:14 -0300

    mock data

--- app/features/steps/test_pest.py
+++ app/features/steps/test_pest.py
@@ -3,32 +3,31 @@ import requests
 
 request_headers = {}
 request_bodies = {}
+response_codes = {}
 api_url = None
 
 
 @given('a pagina de registro da praga')
 def step_impl_given(context):
     global api_url
-    api_url = 'https://smartvit-pest-dev.herokuapp.com/pest'
+    api_url = 'https://smartvit-pest-stg.herokuapp.com/pest'
     print('url :'+api_url)
 
 
 @when('ele registar os campos da praga')
 def step_impl_when(context):
-    request_bodies['POST'] = {"priority": "Alta",
-                              "problem": "Sistema eletronico com defeito",
-                              "description": "sistema esta aquecendo muito, o que fazer"}
+    request_bodies['POST'] = {"idVineyard": "5fad331b38b2670687db57e2",
+                              "type": "cigarras",
+                              "startTime": "12-11-2020"}
     response = requests.post(
-                            'https://smartvit-pest-dev.herokuapp.com/pest',
+                            api_url,
                             json=request_bodies['POST']
                             )
+    statuscode = response.status_code
+    response_codes['POST'] = statuscode
 
 
 @then('os dados devem passar pelo servico atraves do BFF e armazenar no banco')
 def step_impl_then(context):
-    api_bff_url = 'https://smartvit-user-bff-dev.herokuapp.com/pest/'
-    response = requests.post(
-                            api_bff_url,
-                            json=request_bodies['POST']
-                            )
-    assert response.status_code != 200
+    print('Post rep code ;'+str(response_codes['POST']))
+    assert response_codes['POST'] == 200
